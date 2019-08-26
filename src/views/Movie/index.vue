d511669e94a3d9b8d68d1fa1c0581a327f955f3f
MisterZZL <zhangzhenlin322@163.com>
2019-08-27 00:09:45 +0800

    初始化项目，配置路由

---
+++ src/views/Movie/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="main">
+    <Header title="喵喵电影"></Header>
+    <div id="content">
+      <div class="movie_menu">
+        <div class="city_name">
+          <span>大连</span>
+          <i class="iconfont icon-lower-triangle"></i>
+        </div>
+        <div class="hot_swtich">
+          <div class="hot_item active">正在热播</div>
+          <div class="hot_item">即将上映</div>
+        </div>
+        <div class="search_entry">
+          <i class="iconfont icon-sousuo"></i>
+        </div>
+      </div>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </div>
+    <TabBar></TabBar>
+  </div>
+</template>
+<script>
+import Header from "../../components/Header";
+import TabBar from "../../components/TabBar";
+export default {
+  name: "Movie",
+  components: {
+    Header,
+    TabBar
+  }
+};
+</script>
+<style>
+</style>
