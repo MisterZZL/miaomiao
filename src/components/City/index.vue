18edfa57ac338f3fe495ff5ed76f2d78ec89b4a3
MisterZZL <zhangzhenlin322@163.com>
2019-08-29 17:51:10 +0800

    Signed-off-by: MisterZZL <zhangzhenlin322@163.com> 城市列表数据的获取，右侧的字母还，欸处理

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,126 +1,107 @@
 <template>
-  <div data-v-679f5e5e data-v-7fbd612e class="city_body">
-    <div data-v-679f5e5e class="city_list">
-      <div data-v-3c6eb66d data-v-679f5e5e class="wrapper">
-        <div data-v-679f5e5e data-v-3c6eb66d>
-          <div data-v-679f5e5e data-v-3c6eb66d class="city_hot">
-            <h2 data-v-679f5e5e data-v-3c6eb66d>热门城市</h2>
-            <ul data-v-679f5e5e data-v-3c6eb66d class="clearfix">
-              <li data-v-679f5e5e data-v-3c6eb66d>北京</li>
-              <li data-v-679f5e5e data-v-3c6eb66d>上海</li>
-              <li data-v-679f5e5e data-v-3c6eb66d>广州</li>
-              <li data-v-679f5e5e data-v-3c6eb66d>天津</li>
-              <li data-v-679f5e5e data-v-3c6eb66d>杭州</li>
-              <li data-v-679f5e5e data-v-3c6eb66d>南京</li>
-              <li data-v-679f5e5e data-v-3c6eb66d>成都</li>
+  <div class="city_body">
+    <div class="city_list">
+      <div class="wrapper">
+        <div>
+          <div class="city_hot">
+            <h2>热门城市</h2>
+            <ul class="clearfix">
+              <li v-for="item in hotList" :key="item.index">{{item.nm}}</li>
             </ul>
           </div>
-          <div data-v-679f5e5e data-v-3c6eb66d class="city_sort">
-            <div data-v-679f5e5e data-v-3c6eb66d>
-              <h2 data-v-679f5e5e data-v-3c6eb66d>A</h2>
-              <ul data-v-679f5e5e data-v-3c6eb66d>
-                <li data-v-679f5e5e data-v-3c6eb66d>阿拉善盟</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>鞍山</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安庆</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安阳</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>阿坝</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安顺</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安康</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>阿勒泰</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>阿克苏</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安吉</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安丘</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安岳</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安平</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安宁</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安溪</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安化</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>阿勒泰市</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安福</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>阿荣旗</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安陆市</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>安州区</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>阿城区</li>
-              </ul>
-            </div>
-            <div data-v-679f5e5e data-v-3c6eb66d>
-              <h2 data-v-679f5e5e data-v-3c6eb66d>B</h2>
-              <ul data-v-679f5e5e data-v-3c6eb66d>
-                <li data-v-679f5e5e data-v-3c6eb66d>北京</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>保定</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>蚌埠</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>包头</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>本溪</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>巴彦淖尔</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>白城</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>白山</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>亳州</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>滨州</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>巴中</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>北海</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>百色</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>毕节</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>保山</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>宝鸡</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>白银</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>巴州</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>博尔塔拉</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>滨海</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>宝应</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>北流</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>博爱</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>北碚</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>宝丰</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>泌阳</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>博兴</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>博山</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>璧山</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>彬县</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>宾阳</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>博白县</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>博罗县</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>北镇市</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>泊头市</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>北安市</li>
-                <li data-v-679f5e5e data-v-3c6eb66d>巴彦县</li>
+          <div class="city_sort">
+            <div v-for="item in cityList" :key="item.id">
+              <h2 >{{item.index}}</h2>
+              <ul >
+                <li v-for="it in item.list" :key="it.id">{{it.nm}}</li>
               </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div data-v-679f5e5e class="city_index">
-      <ul data-v-679f5e5e>
-        <li data-v-679f5e5e>A</li>
-        <li data-v-679f5e5e>B</li>
-        <li data-v-679f5e5e>C</li>
-        <li data-v-679f5e5e>D</li>
-        <li data-v-679f5e5e>E</li>
-        <li data-v-679f5e5e>F</li>
-        <li data-v-679f5e5e>G</li>
-        <li data-v-679f5e5e>H</li>
-        <li data-v-679f5e5e>J</li>
-        <li data-v-679f5e5e>K</li>
-        <li data-v-679f5e5e>L</li>
-        <li data-v-679f5e5e>M</li>
-        <li data-v-679f5e5e>N</li>
-        <li data-v-679f5e5e>P</li>
-        <li data-v-679f5e5e>Q</li>
-        <li data-v-679f5e5e>R</li>
-        <li data-v-679f5e5e>S</li>
-        <li data-v-679f5e5e>T</li>
-        <li data-v-679f5e5e>W</li>
-        <li data-v-679f5e5e>X</li>
-        <li data-v-679f5e5e>Y</li>
-        <li data-v-679f5e5e>Z</li>
+    <div class="city_index">
+      <ul>
+        <li v-for="item in cityList" :key="item.id">{{item.index}}</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import { city } from "../../api/city";
 export default {
-  name: "City"
+  name: "City",
+  data() {
+    return {
+      cityList: [],
+			hotList: [],
+    };
+  },
+  methods: {
+    getCity() {
+      let cities=[]
+      city().then(res => {
+        if (res.status === 0) {
+          cities = res.data.cities;
+          let {cityList,hotList} = this.formatCityList(cities);
+          this.cityList = cityList;
+					this.hotList = hotList;
+          console.log(this.cityList);
+        }
+      });
+    },
+    //格式话城市列表
+    formatCityList(cities) {
+			var cityList = [];
+			var hotList = [];
+			for(var i=0;i<cities.length;i++) {
+				if(cities[i].isHot === 1) {
+					hotList.push(cities[i])
+				}
+			}
+			for(var i=0;i<cities.length;i++) {
+				var firstLetter = cities[i].py.substring(0,1).toUpperCase()
+				if(toCom(firstLetter)) {
+					cityList.push({index:firstLetter,list:[{nm:cities[i].nm,id:cities[i].id}]})
+				}else {
+					for(var j=0;j<cityList.length;j++) {
+						if(cityList[j].index === firstLetter) {
+							cityList[j].list.push({nm:cities[i].nm,id:cities[i].id})
+						}
+					}
+				}
+			}
+
+			cityList.sort((n1,n2) => {
+				if (n1.index > n2.index) {
+					return 1
+				}else if(n1.index < n2.index) {
+					return -1
+				}else {
+					return 0
+				}
+			})
+
+			function toCom(firstLetter) {
+				for(var i=0;i<cityList.length;i++) {
+					if (cityList[i].index === firstLetter) {
+						return false
+					}
+				}
+				return true
+			}
+			// console.log(cityList);
+			// console.log(hotList);
+			return {
+				cityList,
+				hotList
+			}
+		},
+  },
+  mounted() {
+    this.getCity();
+  }
 };
 </script>
 <style lang="scss" scoped>
