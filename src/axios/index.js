89d39a25ca4940b42180dd94ea3ad31da06c15c3
MisterZZL <zhangzhenlin322@163.com>
2019-09-01 22:47:47 +0800

    Signed-off-by: MisterZZL <zhangzhenlin322@163.com> 完成搜索页面（axiso取消多次请求）

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,31 +1,30 @@
 import axios from "axios"
 
-
-
 class AjaxRequest {
+
     constructor() {
         //this.baseURL = process.env.NODE_ENV === "production" ? "/" : " http://39.97.33.178";
         this.baseURL = process.env.API_HOST;
         this.timeout = 3000; // 超时
-        this.headers = {
-            'Content-Type': 'application/json;charset=utf-8'
-        };
-        this.withCredentials = true
     }
+
     //合并参数
     merge(options) {
-        return {
-            ...options,
-            baseURL: this.baseURL,
-            timeout: this.timeout
-        };
-    }
-    //拦截器
-    setInterceptor(instance, url) {
-        instance.interceptors.request.use((config) => {
+            // console.log(this._pending)
+            return {
+                ...options,
+                baseURL: this.baseURL,
+                timeout: this.timeout,
 
+            };
+        }
+        //拦截器
+    setInterceptor(instance, url) {
+        //请求前拦截
+        instance.interceptors.request.use((config) => {
             return config;
         });
+        //响应拦截
         instance.interceptors.response.use((res) => {
 
             return res.data;
--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import router from './routers'
 import store from './stores'
 
 
+import axios from 'axios'
+Vue.prototype.axios = axios;
+
 Vue.config.productionTip = false
 
 /* 请求图片url的过滤器 */
-Vue.filter("setWH",(url,arg)=>{
-    return url.replace(/w\.h/,arg)
+Vue.filter("setWH", (url, arg) => {
+    return url.replace(/w\.h/, arg)
 })
 
 new Vue({
