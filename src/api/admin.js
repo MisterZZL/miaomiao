d599a6c80c200cfc2c23ad0bf4e9a1caf5273582
MisterZZL <zhangzhenlin322@163.com>
2019-09-24 00:13:07 +0800

    Signed-off-by: MisterZZL <zhangzhenlin322@163.com> 后台数据获取成功

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,9 +1,17 @@
 import axios from '../axios/index'
 
-// 进入管理员页面
+// 进入管理员页面,获取管理员信息
 export const getAdmin = () => {
     return axios.request({
         url: "/api2/admin",
+        method: "get"
+    });
+}
+
+// 进入后台管理页面，获取用户列表
+export const get_userList = () => {
+    return axios.request({
+        url: "/api2/admin/userList",
         method: "get"
     });
 }
--- src/views/Admin/index.vue
+++ src/views/Admin/index.vue
@@ -1,14 +1,44 @@
 <template>
   <div id="main">
     <el-container>
-  <el-header>喵喵网后台管理系统
-    <span class="username">{{$store.state.login.username}}</span>
-  </el-header>
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-main>Main</el-main>
-  </el-container>
-</el-container>
+      <el-header>
+        喵喵网后台管理系统
+        <div class="username">
+          <el-avatar
+            class="userImg"
+            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+          ></el-avatar>
+          {{$store.state.login.username}}
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+
+          <el-menu default-active="2" class="el-menu-vertical-demo">
+
+            <el-menu-item index="1">
+              <i class="el-icon-location"></i>
+              <router-link to="/admin/users" tag="span">用户管理</router-link>
+            </el-menu-item>
+
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <router-link to="/admin/movie" tag="span">电影管理</router-link>
+            </el-menu-item>
+
+            <el-menu-item index="3">
+              <i class="el-icon-document"></i>
+              <router-link to="/admin/cinema" tag="span">影院管理</router-link>
+            </el-menu-item>
+
+          </el-menu>
+
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -18,11 +48,19 @@ import { getAdmin } from "../../api/admin";
 
 export default {
   name: "admin",
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  },
   beforeRouteEnter(to, from, next) {
     // ...
     getAdmin().then(res => {
       if (res.status === 0) {
-        console.log("进后台")
+        console.log("进后台");
         next();
       } else {
         next("/mine/login");
@@ -33,9 +71,16 @@ export default {
 </script>
 
 <style lang="scss" scope>
-.username{
+.username {
   float: right;
   margin-right: 10px;
+  height: 100%;
+  position: relative;
+  .userImg {
+    position: absolute;
+    right: 20px;
+    top: 10px;
+  }
 }
 .el-header,
 .el-footer {
@@ -56,11 +101,10 @@ export default {
   background-color: #e9eef3;
   color: #333;
   text-align: center;
-  line-height: 160px;
+
 }
 
 body > .el-container {
   margin-bottom: 40px;
 }
-
 </style>
